ff2906c7d8899528a2d16e46add3b2eaae16c1c1
HelloKowshik <hellokowshik@gmail.com>
2020-10-29 02:24:00 +0600

    some bugs fixed

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import UsersComponent from './Components/UsersComponent/UsersComponent';
+import AdminsComponents from './Components/AdminsComponent/AdminsComponents';
+import NoMatch from './Components/NoMatch/NoMatch';
+import Login from './Components/Login/Login';
+import { createContext } from 'react';
+import { useState } from 'react';
+import PrivateRoute from './Components/PrivateRoute/PrivateRoute';
+
+export const UserContext = createContext();
+
+
+function App() {
+  const [loggedInUser, setLoggedInUser] = useState({});
+
+  return (
+    <UserContext.Provider value={[loggedInUser, setLoggedInUser]}>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <PrivateRoute path="/usersComponent/:project">
+            <UsersComponent />
+          </PrivateRoute>
+          <PrivateRoute path="/adminsComponents">
+            <AdminsComponents />
+          </PrivateRoute>
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
---
+++ src/Components/Login/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyArTtWaRye0Ge5BMp8GyOwEm4DRiP24u0Y",
+  authDomain: "creative-agency-web-app.firebaseapp.com",
+  databaseURL: "https://creative-agency-web-app.firebaseio.com",
+  projectId: "creative-agency-web-app",
+  storageBucket: "creative-agency-web-app.appspot.com",
+  messagingSenderId: "712453528449",
+  appId: "1:712453528449:web:f87e09de59f14371016680"
+};
+
+export default firebaseConfig;
